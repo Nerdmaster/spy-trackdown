68b4049a7fd85326c5e2d160b61aad9ec431699d
Nerdmaster <brody76@gmail.com>
2013-10-10 21:15:11 -0700

    Enable twig

--- root.php
+++ root.php
@@ -12,6 +12,7 @@ function require_class($name) {
 
 // Include config as all parts of the app need it
 require_once("config.inc.php");
+require_once(ROOT . '/vendor/autoload.php');
 require_class("Map");
 
 // Set up a global database connection to simplify other scripts
@@ -19,6 +20,8 @@ $DB = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
 if ($DB->connect_error) {
   die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
+
+Twig_Autoloader::register();
 
 // Set up map data
 Map::initialize();
