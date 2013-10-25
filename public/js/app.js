208df7fdfc7be10c21d3e6f1198f92aa6df0d1ff
Nerdmaster <brody76@gmail.com>
2013-10-24 20:38:29 -0700

    Refactor JS
    - Centralizes pulling command from phone
    - Puts refreshing of phone display into functions which alter the
      display to avoid copying the pattern of alter + refresh

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,33 @@
+// Returns the currently entered command on the phone
+function currentCommand() {
+  return $("#phone-display").data("command");
+}
+
 // Displays current command in phone "terminal"
 function displayPhoneCommand() {
-  var command = $("#phone-display").data("command");
-  $("#phone-display").html(command + "_");
+  $("#phone-display").html(currentCommand() + "_");
 }
 
 // Resets the phone command to a blank string
 function resetCommand() {
   $("#phone-display").data("command", "");
+  displayPhoneCommand();
 }
 
 // Processes button presses, adding to the command string and showing a confirmation button
 // if the string maps to a full command
 function addToCommand(val) {
-  var command = $("#phone-display").data("command");
-  $("#phone-display").data("command", command + val);
-  // TODO: Do some lookup for commands, and if it's valid offer a "confirm" button
+  $("#phone-display").data("command", currentCommand() + val);
+  displayPhoneCommand();
 }
 
 $(function() {
   // Attach listeners to phone buttons
   $("#phone .phone-button.command button").on("click", function() {
     addToCommand($(this).data("value"));
-    displayPhoneCommand();
   });
+
   $("#phone .phone-button.reset button").on("click", function() {
     resetCommand();
-    displayPhoneCommand();
   });
 });
